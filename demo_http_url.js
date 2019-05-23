b5bef4114d65a21cbd92973bb904b52f2f4f7e8d
Ikenna Victor <ikennavictorofficial@gmail.com>
2019-05-23 21:16:46 +0100

    added http modules and file system

--- demo_http_url.js
+++ demo_http_url.js
@@ -2,5 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  //Return the url part of the request object:
+  res.write(req.url);
+  res.end();
 }).listen(8080);
---
+++ demo_querystring.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  /*Use the url module to turn the querystring into an object:*/
+  var q = url.parse(req.url, true).query;
+  /*Return the year and month from the query object:*/
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+}).listen(8080);
