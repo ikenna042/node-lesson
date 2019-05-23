ce89307e2ce9b9b3f44d90bcecff7692a453e075
Ikenna Victor <ikennavictorofficial@gmail.com>
2019-05-23 11:17:01 +0100

    added code to use my first node mode in the server to say the date and time.

--- first.js
+++ first.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
+}).listen(8080); 
